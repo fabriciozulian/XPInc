d6badef086646a6af8bd83f1a431465ea5f29b67
Fabricio Zulian <fabriciozulian@outlook.com>
2024-08-29 23:12:47 -0300

    Estudando Exceções em CSharp

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿try
+{
+string[] linhas = File.ReadAllLines("Arquivos/arquivoLeitura.txt");
+
+foreach (string line in linhas)
+{
+    System.Console.WriteLine(line);
+}
+}
+catch (Exception ex)
+{
+    System.Console.WriteLine($"Ocorreu uma exceção genérica. {ex.Message}");
+}
